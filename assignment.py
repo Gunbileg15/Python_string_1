bfd3334ff8f5c877a86b190a26ab98894ea60998
Gunbileg15 <ya.gunbileg@gmail.com>
2026-09-21 17:23:44 +0800

    Implement character counting and text manipulation functions

--- assignment.py
+++ assignment.py
@@ -2,30 +2,77 @@
 
 # Exercise 1
 def count_characters(text):
-    # Write your code here
+n=str(input())
+m=len(n)
+print(m)
     pass
 
 # Exercise 2
 def remove_spaces(text):
-    # Write your code here
+n=input()
+l = ""
+for c in n:
+    if c!=" ":
+        l+=c
+print(l)
     pass
 
 # Exercise 3
 def count_vowels(text):
-    # Write your code here
+n=input()
+count1=0
+count2=0
+count3=0
+count4=0
+count5=0
+count=0
+for c in n:
+    if c=="a":
+        count1+=1
+    elif c=="e":
+        count2+=1
+    elif c=="i":
+        count3+=1
+    elif c=="u":
+        count4+=1
+    elif c=="o":
+        count5+=1
+count=count1+count2+count3+count4+count5
+print(count)
     pass
 
 # Exercise 4
 def replace_vowels(text):
-    # Write your code here
+n = input()
+vowels = "aeiouAEIOU"
+l = ""
+
+for c in n:
+    if c in vowels:
+        l += "*"
+    else:
+        l += c
+print(l)
     pass
 
 # Exercise 5
 def count_words(text):
-    # Write your code here
+n=input()
+m=n.split()
+g=len(m)
+print(g)
     pass
 
 # Exercise 6
 def find_longest_word(text):
-    # Write your code here
+n = input()
+words = n.split()
+
+longest = words[0]
+
+for i in words:
+    if len(i) > len(longest):
+        longest = i
+
+print(longest)
     pass
